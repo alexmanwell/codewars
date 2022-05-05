99b259682064976d5cff4f992ee6164ecbe8a71f
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-05-05 15:54:30 +0300

    solve task Text align justify
    Need to make satisfying performance.

---
+++ js/TextAlignJustify.js
@@ -0,0 +1,59 @@
+/* TODO: need to make satisfying performance */
+
+const separate = (words, width) => {
+    let line = [];
+    let list = [];
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        let length = line.join("").length;
+        if (length + word.length + 1 < width) {
+            line.push(word + " ");
+        } else if ((length + word.length + 1 === width) || (length + word.length === width))  {
+            line.push(word.trim());
+            list.push(line);
+            line = [];
+        } else if ((length + word.length + 1 > width) || length + word.length > width) {
+            const lastWord = line.pop();
+            line.push(lastWord.trim());
+            list.push(line);
+            line = [word + " "];
+        }
+        if (i === words.length - 1 && line.length !== 0) {
+            const lastWord = line.pop();
+            line.push(lastWord.trim());
+            list.push(line);
+        }
+    }
+
+    return list;
+};
+
+const justifyText = (list, width) => {
+    let text = "";
+    for (let row = 0; row < list.length - 1; row++) {
+        const words = list[row];
+        let length = words.join("").length;
+        while (length < width) {
+            for (let i = 0; i < words.length - 1; i++) {
+                words[i] = words[i] + " ";
+                length = words.join("").length;
+                if (length === width) {
+                    break;
+                }
+            }
+        }
+        text += words.join("") + "\n";
+    }
+    text += list[list.length - 1].join("");
+
+    return text;
+};
+
+function justify(text, width) {
+    const words = text.split(" ");
+    if (words.length === 1 || words.length === 0) {
+        return text;
+    }
+
+    return justifyText(separate(words, width), width);
+}
