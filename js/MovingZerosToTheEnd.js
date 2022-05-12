1d71f4b7196841d48b463e2fb29192fe51047881
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-05-12 21:00:08 +0300

    second sloution kata Moving Zeros To The End

--- js/MovingZerosToTheEnd.js
+++ js/MovingZerosToTheEnd.js
@@ -14,4 +14,21 @@ var moveZeros = function (arr) {
     }
 
     return result;
+};
+
+/* second sloution */
+function moveZeros(arr) {
+    let index = 0;
+    let length = arr.length;
+    let zeros = [];
+    while (index < arr.length) {
+        if (arr[index] === 0) {
+            const removed = arr.splice(index, 1);
+            zeros = [...zeros, ...removed];
+            length -= 1;
+        } else {
+            index++;
+        }
+    }
+    return [...arr, ...zeros];
 }
