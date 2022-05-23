4e4602203ae6bb721c2027fc20870d4e5b06c080
Alexandr Manvelyan <alexandr.manvelyan@yandex.ru>
2022-05-23 19:03:54 +0300

    binary tree compare. Doesn't fail one test.

---
+++ js/BinaryTreeCompare.js
@@ -0,0 +1,30 @@
+function compare(a, b) {
+    if (a === null || b === null) {
+        return false;
+    }
+    if (a.val !== b.val) {
+        return false;
+    }
+
+    const checkInLeft = (a1, b1) => {
+        if (a1 === null || b1 === null) {
+            return !((a1 && !b1) || (!a1 && b1));
+        }
+        if (a1.val !== b1.val) {
+            return false;
+        }
+        return checkInLeft(a1.left, b1.left);
+    };
+
+    const checkInRight = (a1, b1) => {
+        if (a1 === null || b1 === null) {
+            return !((a1 && !b1) || (!a1 && b1));
+        }
+        if (a1.val !== b1.val) {
+            return false;
+        }
+        return checkInRight(a1.right, b1.right);
+    };
+
+    return checkInLeft(a.left, b.left) && checkInRight(a.right, b.right);
+}
